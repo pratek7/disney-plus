4c8effa10c927227d57cce25b4cfe6cccb67aa2d
pratek7 <pratikjungkarki123@gmail.com>
2023-10-04 08:21:29 -0400

    ADD: Viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Viewers = () => {
-  return <div>Viewers</div>;
+  return (
+    <div className="viewersContainer mt-8 grid grid-cols-5 gap-6 px-0 pt-8 pb-6">
+      <div>
+        <img src="/images/viewers-disney.png" alt="" />
+      </div>
+      <div>
+        <img src="/images/viewers-marvel.png" alt="" />
+      </div>
+      <div>
+        <img src="/images/viewers-starwars.png" alt="" />
+      </div>
+      <div>
+        <img src="/images/viewers-national.png" alt="" />
+      </div>
+      <div>
+        <img src="/images/viewers-pixar.png" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Viewers;
