34a2a0f6bd7489e91ea08bb0bdcea77eecd11f9e
pratek7 <pratikjungkarki123@gmail.com>
2023-10-16 02:45:36 -0400

    ADD: added video effect on viewer

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -11,29 +11,32 @@ const Viewers = () => {
       </div>
       <div>
         <img src="/images/viewers-marvel.png" alt="" />
-        <video autoPlay={true} loop={true} playsInline={true}>
-          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
-        </video>
-      </div>
-      <div>
-        <img src="/images/viewers-starwars.png" alt="" />
         <video autoPlay loop playsInline>
           <source src="/videos/1564676115-marvel.mp4" type="video/mp4" />
         </video>
       </div>
       <div>
-        <img src="/images/viewers-national.png" alt="" />
+        <img src="/images/viewers-starwars.png" alt="" />
+
         <video autoPlay={true} loop={true} playsInline={true}>
           <source src="/videos/1608229455-star-wars.mp4" type="video/mp4" />
         </video>
       </div>
       <div>
-        <img src="/images/viewers-pixar.png" alt="" />
+        <img src="/images/viewers-national.png" alt="" />
+
         <video autoPlay={true} loop={true} playsInline={true}>
           <source
             src="/videos/1564676296-national-geographic.mp4"
             type="video/mp4"
           />
+        </video>
+      </div>
+      <div>
+        <img src="/images/viewers-pixar.png" alt="" />
+
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676714-pixar.mp4" type="video/mp4" />
         </video>
       </div>
     </div>
